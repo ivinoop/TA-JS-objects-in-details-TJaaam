358b26f799fb9f79844d81a4bc679545141d4e18
Vinoo <ivinoop@gmail.com>
2021-11-25 12:08:54 +0530

    refactor

--- block-BJaaig/code/index.js
+++ block-BJaaig/code/index.js
@@ -34,16 +34,16 @@ console.log(user2.sayHello()); // output / Hello Arya
 console.log(user.sayHello.call(user2)); // ouput / Hello Arya
 console.log(user.sayHello.call(user2, 'Hey')); // output / Hey Arya
 console.log(user.sayHello.apply(user2, ['Hey'])); // output / Hey Arya
-console.log(typeof user.sayHello.bind(user2)); // output / function
+console.log(typeof user.sayHello.bind(user2)); // output / "function"
 console.log(user.sayHello.bind(user2)()); // output / Hello Arya
 console.log(userSayHello()); // output / Hello undefined
-console.log(typeof userSayHello.bind(user2)); // output / function
+console.log(typeof userSayHello.bind(user2)); // output / "function"
 console.log(userSayHello.bind(user2)()); // output / Hello Arya
 console.log(user3.sayHello()); // error / Uncaught TypeError: user3.sayHello is not a function
-console.log(userSayHello.apply(user3)); // output / error
+console.log(userSayHello.apply(user3)); // output / Hello Bran
 console.log(userSayHello.call(user3)); // output / Hello Bran
-console.log(typeof new MainUser()); // output / object
-console.log(typeof new MainUser()); // output / object
+console.log(typeof new MainUser()); // output / "object"
+console.log(typeof new MainUser); // output / "object"
 console.log(new MainUser().sayHello()); // output / Hello Tyrion
 console.log(new MainUser().sayHello.call(user2)); // output / Hello Arya
 console.log(new MainUser().sayHello.call(user)); // output / Hello John
