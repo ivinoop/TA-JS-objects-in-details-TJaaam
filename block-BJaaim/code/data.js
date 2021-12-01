2cf40a018019c504f04b3b8338eb1ae9303f73e9
Vinoo <ivinoop@gmail.com>
2021-12-01 12:28:13 +0530

    quiz app

--- block-BJaaim/code/data.js
+++ block-BJaaim/code/data.js
@@ -1,105 +1,34 @@
-const questionnire = [
+const questionnaire = [
   {
-      title: "Who was the first female president of India ?",
+      title: "What is ReactJS?",
       options: [
-          "Indira Gandhi",
-          "Sucheta Kripalani",
-          "Pratibha Devi Singh Patil ",
-          "Vijya Lakshmi Pandit"
+          "Framework",
+          "Library",
+          "Runtime",
+          "Database"
       ],
       correctAnswerIndex: 2
   },
 
   {
-      title: "Where is the capital of Kenya ?",
+      title: "When was the first website launched?",
       options: [
-          "Durban",
-          "Nairobi",
-          "Mogadhisu",
-          "Mombasa"
+          "1991",
+          "1987",
+          "1993",
+          "1980"
       ],
       correctAnswerIndex: 1
   },
 
   {
-      title: "What’s the smallest country in the world?",
+      title: "What is the keyword used to point to an object using a function?",
       options: [
-          "Monaco",
-          "Nauru",
-          "Tuvalu",
-          "Vatican City"
+          "let",
+          "const",
+          "this",
+          "while"
       ],
       correctAnswerIndex: 3
   },
-  {
-      title: "How many time zones are there in Russia?",
-      options: [
-          "11",
-          "10",
-          "9",
-          "13"
-      ],
-      correctAnswerIndex: 0
-  },
-  {
-      title: "What country has the most islands in the world?",
-      options: [
-          "Sweden",
-          "Indonesia",
-          "Denmark",
-          "Canda"
-      ],
-      correctAnswerIndex: 0
-  },
-  {
-      title: "Name the longest river in the world?",
-      options: [
-          "Amazon",
-          "Ganga",
-          "Nile",
-          "Rhine"
-      ],
-      correctAnswerIndex: 2
-  },
-  {
-      title: "What city do The Beatles come from?",
-      options: [
-          "London",
-          "Liverpool",
-          "Madrid",
-          "Barcelona"
-      ],
-      correctAnswerIndex: 1
-  },
-  {
-      title: "Who invented the World Wide Web, and when?",
-      options: [
-          "Larry Page, 1990",
-          "Larry Page, 1992",
-          "Tim Berners-Lee, 1992",
-          "Tim Berners-Lee, 1990"
-      ],
-      correctAnswerIndex: 3
-  },
-  {
-      title: "Until 1923, what was the Turkish city of Istanbul called?",
-      options: [
-          "Basutoland",
-          "Constantinopole",
-          "Formosa",
-          "Turicum"
-      ],
-      correctAnswerIndex: 1
-  },
-  {
-      title: "Which Indian city is known as The Venice of The East?",
-      options: [
-          "Kunnur",
-          "Alappuzha",
-          "Kochi",
-          "Kozhikode"
-      ],
-      correctAnswerIndex: 1
-  },
-
 ]
